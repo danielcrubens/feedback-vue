07a83314131260631a1b7e52aee5283f5bea9b06
danielcrubens <dcr.tecnologi@gmail.com>
2024-01-21 10:03:59 -0300

    controlando rotas autenticadas :fire:

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import router from '../router'
 import AuthService from './auth'
+import UsersService from './users'
 
 const API_ENVS = {
   production: '',
@@ -11,6 +13,30 @@ const httpClient = axios.create({
   baseURL: API_ENVS[process.env.NODE_ENV] || API_ENVS.local
 })
 
+httpClient.interceptors.request.use(config => {
+  const token = window.localStorage.getItem('token')
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+httpClient.interceptors.request.use(
+  (response) => response,
+  (error) => {
+    const cantThrowAnError =
+      error.request.status === 0 || error.resquest.status === 500
+    if (cantThrowAnError) {
+      throw new Error(error.message)
+    }
+    if (error.response.status === 401) {
+      router.push({ name: 'Home' })
+    }
+    return error
+  }
+)
+
 export default {
-  auth: AuthService(httpClient)
+  auth: AuthService(httpClient),
+  users: UsersService(httpClient)
 }
